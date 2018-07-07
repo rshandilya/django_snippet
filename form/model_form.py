359a816ab375fa154eeaf30ee11a5ab7e9f5997e
rshandilya <prof.rshandilya@gmail.com>
2018-07-07 06:41:09 +0530

    Update model_form.py

--- form/model_form.py
+++ form/model_form.py
@@ -78,3 +78,18 @@ def contact(request):
                 # Insert into DB
                 pending_contact.save()
 
+## SNIPPET 4 ; REF DJANGOGIRLS #######
+##### EDITING USING MODEL FORM  ########
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
