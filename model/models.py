e9c8dad764ca7d1db05f909a69f5235426b736f7
rshandilya <prof.rshandilya@gmail.com>
2018-07-11 05:34:49 +0530

    Update models.py

--- model/models.py
+++ model/models.py
@@ -1,1 +1,9 @@
 # MODELS SNIPPETS
+
+##### UNIQUE TOGETHER  #####
+class MyModel(models.Model):
+  field1 = models.CharField(max_length=50)
+  field2 = models.CharField(max_length=50)
+
+  class Meta:
+    unique_together = ('field1', 'field2',)
