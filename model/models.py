307d42d35f2e8813b68fb86979c604a5cfe284e7
rshandilya <prof.rshandilya@gmail.com>
2018-07-20 18:57:47 +0530

    Update models.py

--- model/models.py
+++ model/models.py
@@ -1,5 +1,29 @@
 # MODELS SNIPPETS
 
+#### MANYTOMANY FIELD #######
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+    def __str__(self):
+        return self.name
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
+
+
+
+
+
 ##### UNIQUE TOGETHER  #####
 class MyModel(models.Model):
   field1 = models.CharField(max_length=50)
