e720c867e61ec86b3c378d619a84f155ba00162a
rshandilya <prof.rshandilya@gmail.com>
2018-08-17 21:55:24 +0530

    Update generic_edit_views.py

--- view/generic_edit_views.py
+++ view/generic_edit_views.py
@@ -298,4 +298,39 @@ class ItemCreation(SuccessMessageMixin,CreateView):
     success_url = reverse_lazy('items:index')
     success_message = "Item %(name)s created successfully"
 
-      
+################# AJAX #################
+from django.http import JsonResponse
+from django.views.generic.edit import CreateView
+from myapp.models import Author
+
+class AjaxableResponseMixin:
+    """
+    Mixin to add AJAX support to a form.
+    Must be used with an object-based FormView (e.g. CreateView)
+    """
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+        else:
+            return response
+
+    def form_valid(self, form):
+        # We make sure to call the parent's form_valid() method because
+        # it might do some processing (in the case of CreateView, it will
+        # call form.save() for example).
+        response = super().form_valid(form)
+        if self.request.is_ajax():
+            data = {
+                'pk': self.object.pk,
+            }
+            return JsonResponse(data)
+        else:
+            return response
+
+class AuthorCreate(AjaxableResponseMixin, CreateView):
+    model = Author
+    fields = ['name']
+                                    
+                                    
+                                    
