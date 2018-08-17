86dbd21f6249d592ca0b8f235579f060eb038c4a
rshandilya <prof.rshandilya@gmail.com>
2018-08-17 18:16:42 +0530

    Update generic_views.py

--- view/generic_views.py
+++ view/generic_views.py
@@ -167,3 +167,22 @@ class AuthorDetailView(DetailView):
         obj.save()
         return obj
     
+
+##### RedirectView #######
+    
+from django.shortcuts import get_object_or_404
+from django.views.generic.base import RedirectView
+
+from articles.models import Article
+
+class ArticleCounterRedirectView(RedirectView):
+
+    permanent = False
+    query_string = True
+    pattern_name = 'article-detail'
+
+    def get_redirect_url(self, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs['pk'])
+        article.update_counter()
+        return super().get_redirect_url(*args, **kwargs)
+    
