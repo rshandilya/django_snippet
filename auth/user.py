8049564ab0edde2c12b1971a55eaa841dad9f240
rshandilya <prof.rshandilya@gmail.com>
2018-08-22 10:58:40 +0530

    Update user.py

--- auth/user.py
+++ auth/user.py
@@ -22,3 +22,61 @@ class CustomUserAdmin(UserAdmin):
                }),
     )
 admin.site.register(User, CustomUserAdmin)    
+
+##########################################################
+AUTH_USER_MODEL = 'users.CustomUser'
+
+# users/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    # add additional fields in here
+
+    def __str__(self):
+        return self.email
+    
+# users/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email')
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email')
+        
+# users/admin.py
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username',]
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+# users/views.py
+from django.urls import reverse_lazy
+from django.views import generic
+
+from .forms import CustomUserCreationForm
+
+class SignUp(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
