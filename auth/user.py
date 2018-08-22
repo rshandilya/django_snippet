831dcbd117b6700812317098495a9ca37c67bf2d
rshandilya <prof.rshandilya@gmail.com>
2018-08-22 10:31:23 +0530

    Update user.py

--- auth/user.py
+++ auth/user.py
@@ -14,3 +14,11 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 admin.site.register(User, UserAdmin)
+
+## For extra userfield
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('field_type',),
+               }),
+    )
+admin.site.register(User, CustomUserAdmin)    
