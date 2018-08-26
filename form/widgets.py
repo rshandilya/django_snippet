886147bc8e0593c2a2fe3b8cd728fefc1e133acd
rshandilya <prof.rshandilya@gmail.com>
2018-08-26 16:08:35 +0530

    Update widgets.py

--- form/widgets.py
+++ form/widgets.py
@@ -4,3 +4,45 @@
   {{ choice.choice_label }}
   <span class="radio">{{ choice.tag }}</span>
 {% endfor %}
+
+###############################
+from django import forms
+
+BIRTH_YEAR_CHOICES = ('1980', '1981', '1982')
+FAVORITE_COLORS_CHOICES = (
+    ('blue', 'Blue'),
+    ('green', 'Green'),
+    ('black', 'Black'),
+)
+
+class SimpleForm(forms.Form):
+    birth_year = forms.DateField(widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES))
+    favorite_colors = forms.MultipleChoiceField(
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        choices=FAVORITE_COLORS_CHOICES,
+    )
+
+    
+########## CUSTOMIZING WIDGET INSTANCE ##############
+
+class CommentForm(forms.Form):
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'special'}))
+    url = forms.URLField()
+    comment = forms.CharField(widget=forms.TextInput(attrs={'size': '40'}))
+
+##### in forms #####
+class CommentForm(forms.Form):
+    name = forms.CharField()
+    url = forms.URLField()
+    comment = forms.CharField()
+
+    name.widget.attrs.update({'class': 'special'})
+    comment.widget.attrs.update(size='40')
+
+## in form definition    
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'special'})
+        self.fields['comment'].widget.attrs.update(size='40')
