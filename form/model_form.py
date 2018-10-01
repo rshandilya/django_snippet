ca3023fa5c008a77c6025384663c312bb76fd2a5
rshandilya <prof.rshandilya@gmail.com>
2018-10-01 10:36:25 +0530

    Update model_form.py

--- form/model_form.py
+++ form/model_form.py
@@ -1,4 +1,4 @@
-###### SNIPPET 1 #######
+###### SNIPPET 1 ####### --daniel Rubio
 
 # models.py
 class Contact(models.Model):
@@ -30,17 +30,11 @@ def contact(request):
             return render(request,'about/contact.html',{'form':form})
 
 
+###### MODEL FORM FOR RELATED MODELS ########### --Danile Rubio
 
 from django import forms
 from django.conf import settings
-class Contact(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, default=None)
-    name = models.CharField(max_length=50,blank=True)
-    email = models.EmailField()
-    comment = models.CharField()
 
-from django import forms
-from django.conf import settings
 class Contact(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, default=None)
     name = models.CharField(max_length=50,blank=True)
@@ -80,6 +74,7 @@ def contact(request):
 
 ## SNIPPET 4 ; REF DJANGOGIRLS #######
 ##### EDITING USING MODEL FORM  ########
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -93,3 +88,92 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+######### DJANGO DOC  ########
+
+## Save() methods
+>>> from myapp.models import Article
+>>> from myapp.forms import ArticleForm
+>>> f = ArticleForm(request.POST) # Create a form instance from POST data
+>>> new_article = f.save()   # Save a new Article object from the form's data.
+# Create a form to edit an existing Article, but use
+# POST data to populate the form.
+>>> a = Article.objects.get(pk=1)
+>>> f = ArticleForm(request.POST, instance=a)
+>>> f.save()
+
+##### for related model
+>>> f = AuthorForm(request.POST)
+>>> new_author = f.save(commit=False)
+>>> new_author.some_field = 'some_value'
+>>> new_author.save() # Save the new instance.   
+# Now, save the many-to-many data for the form.
+>>> f.save_m2m()
+
+
+
+###### Overriding the default fields #######
+from django.forms import ModelForm, Textarea
+from myapp.models import Author
+from django.utils.translation import gettext_lazy as _
+
+class AuthorForm(ModelForm):
+    class Meta:
+        model = Author
+        fields = ('name', 'title', 'birth_date')
+        widgets = {
+            'name': Textarea(attrs={'cols': 80, 'rows': 20}),
+        }
+        labels = {
+            'name': _('Writer'),
+        }
+        help_texts = {
+            'name': _('Some useful help text.'),
+        }
+        error_messages = {
+            'name': {
+                'max_length': _("This writer's name is too long."),
+            },
+        }
+
+#### to specify classes ###
+class ArticleForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['pub_date', 'headline', 'content', 'reporter', 'slug']
+        field_classes = {
+            'slug': MySlugFormField,
+        }
+
+#### to have comeplte control  ####
+class ArticleForm(ModelForm):
+    slug = CharField(validators=[validate_slug])
+
+    class Meta:
+        model = Article
+        fields = ['pub_date', 'headline', 'content', 'reporter', 'slug']
+
+##### Form Inheritance #####
+>>> class EnhancedArticleForm(ArticleForm):
+...     def clean_pub_date(self):           # extra validation and cleaning for pub_date field
+...         ...
+
+>>> class RestrictedArticleForm(EnhancedArticleForm):
+...     class Meta(ArticleForm.Meta):       # Inheriting Meta Class
+...         exclude = ('body',)
+
+### Providing initial values
+>>> article = Article.objects.get(pk=1)
+>>> article.headline
+'My headline'
+>>> form = ArticleForm(initial={'headline': 'Initial headline'}, instance=article)
+>>> form['headline'].value()
+'Initial headline'      # initial args override the instance value
+
+
+
+
+
+
+
+
