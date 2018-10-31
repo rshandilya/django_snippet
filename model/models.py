08bc23cc17ec953140d9b065ec2213ab3902fdec
rshandilya <prof.rshandilya@gmail.com>
2018-10-31 12:08:41 +0530

    Update models.py

--- model/models.py
+++ model/models.py
@@ -193,4 +193,29 @@ class VideoManager(models.Manager):
         return super(VideoManager, self).create(**kwargs)
 #===========================================================    
     
-    
+##########  SLUGIFY  ##########
+from django.db import models
+from django.utils.text import slugify
+ 
+ 
+class Article(models.Model):
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(max_length=140, unique=True)
+    content = models.TextField()
+ 
+    def __str__(self):
+        return self.title
+ 
+    def _get_unique_slug(self):
+        slug = slugify(self.title)
+        unique_slug = slug
+        num = 1
+        while Article.objects.filter(slug=unique_slug).exists():
+            unique_slug = '{}-{}'.format(slug, num)
+            num += 1
+        return unique_slug
+ 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self._get_unique_slug()
+        super().save(*args, **kwargs)
