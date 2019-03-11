77ca76a4eb07dd48055b10b62c030bbc5217e8f0
Rahul Shandilya <prof.rshandilya@gmail.com>
2019-03-11 22:28:03 +0530

    Update custom_widget.py

--- form/custom_widget.py
+++ form/custom_widget.py
@@ -7,3 +7,13 @@ class PlaceholderInput(forms.widgets.Input):
         context['widget']['attrs']['maxlength'] = 50
         context['widget']['attrs']['placeholder'] = name.title()
         return context
+    
+# about/placeholder.html
+"""
+<input type="{{ widget.type }}" name="{{ widget.name }}"{% if widget.value != None %}
+value="{{ widget.value }}"{% endif %}{% include "django/forms/widgets/attrs.html" %} />
+
+# django/forms/widgets/attrs.html
+{% for name, value in widget.attrs.items %}{% if value is not False %} {{ name }}{% if value
+is not True %}="{{ value }}"{% endif %}{% endif %}{% endfor %}
+"""
