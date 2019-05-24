c4dfb4ee1911d6aacd5b9abcdd11ead723037676
Rahul Shandilya <prof.rshandilya@gmail.com>
2019-05-25 00:04:04 +0530

    Update serializer.py

--- django_rest/serializer.py
+++ django_rest/serializer.py
@@ -57,23 +57,12 @@ toy1 = Toy(name='Snoopy talking action figure',
            toy_category='Action figures',
            was_included_in_home=False)
 toy1.save()
-toy2 = Toy(name='Hawaiian Barbie',
-           description='Barbie loves Hawaii',
-           release_date=toy_release_date, toy_category='Dolls',
-           was_included_in_home=True)
-toy2.save()
 
 serializer_for_toy1 = ToySerializer(toy1)
 print(serializer_for_toy1.data)
-
-serializer_for_toy2 = ToySerializer(toy2)
-print(serializer_for_toy2.data)
-
 json_renderer = JSONRenderer()
 toy1_rendered_into_json = json_renderer.render(serializer_for_toy1.data)
-toy2_rendered_into_json = json_renderer.render(serializer_for_toy2.data)
 print(toy1_rendered_into_json)
-print(toy2_rendered_into_json)
 
 json_string_for_new_toy = '{"name":"Clash Royale play set","description":"6 figures from Clash Royale", "release_date":"2017-10-09T12:10:00.776594Z","toy_category":"Playset","was_included_in_home":false}'
 json_bytes_for_new_toy = bytes(json_string_for_new_toy, encoding="UTF-8")
