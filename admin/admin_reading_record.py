05fe980536b3b5330c8c72f15b972bd75546ab0f
Rahul Shandilya <prof.rshandilya@gmail.com>
2019-06-02 18:31:04 +0530

    Create admin_reading_record.py

---
+++ admin/admin_reading_record.py
@@ -0,0 +1,25 @@
+
+from django.contrib import admin
+from coffeehouse.stores.models import Store
+
+# Option 1 - Basic
+admin.site.register(Store)
+
+# Option 2 - Allows customizing Django admin behavior
+class StoreAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Store, StoreAdmin)
+
+# Option 3 – Decorator
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    pass
+
+########## LIST DISPLAY 
+    
+from django.contrib import admin
+from coffeehouse.stores.models import Store
+class StoreAdmin(admin.ModelAdmin):
+list_display = ['name','address','city','state']
+admin.site.register(Store, StoreAdmin)    
