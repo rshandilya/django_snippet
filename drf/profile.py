6617cd41b87f326b3e185a88eff51227cde4ba21
Rahul Shandilya <prof.rshandilya@gmail.com>
2020-05-05 22:22:31 +0530

    Create profile.py

---
+++ drf/profile.py
@@ -0,0 +1,253 @@
+from django.db import models
+#from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import uuid
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+
+# Create your models here.
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, password=None):
+        """
+        Create and return a `User` with an email, username and password.
+        """
+        if not email:
+            raise ValueError('Users Must Have an email address')
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Create and return a `User` with superuser (admin) permissions.
+        """
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True
+        )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    # Tells Django that the UserManager class defined above should manage
+    # objects of this type.
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        '''
+        to set table name in database
+        '''
+        db_table = "login"
+
+
+class UserProfile(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=50, unique=False)
+    last_name = models.CharField(max_length=50, unique=False)
+    phone_number = models.CharField(max_length=10, unique=True, null=False, blank=False)
+    age = models.PositiveIntegerField(null=False, blank=False)
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    class Meta:
+        '''
+        to set table name in database
+        '''
+        db_table = "profile"
+        
+        
+#### Serializations.py
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import User, UserProfile
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import update_last_login
+from rest_framework_jwt.settings import api_settings
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserProfile
+        fields = ('first_name', 'last_name', 'phone_number', 'age', 'gender')
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+
+    profile = UserSerializer(required=False)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'profile')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
+        user = User.objects.create_user(**validated_data)
+        UserProfile.objects.create(
+            user=user,
+            first_name=profile_data['first_name'],
+            last_name=profile_data['last_name'],
+            phone_number=profile_data['phone_number'],
+            age=profile_data['age'],
+            gender=profile_data['gender']
+        )
+        return user
+
+
+JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
+JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
+
+
+
+class UserLoginSerializer(serializers.Serializer):
+
+    email = serializers.CharField(max_length=255)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        email = data.get("email", None)
+        password = data.get("password", None)
+        user = authenticate(email=email, password=password)
+        if user is None:
+            raise serializers.ValidationError(
+                'A user with this email and password is not found.'
+            )
+        try:
+            payload = JWT_PAYLOAD_HANDLER(user)
+            jwt_token = JWT_ENCODE_HANDLER(payload)
+            update_last_login(None, user)
+        except User.DoesNotExist:
+            raise serializers.ValidationError(
+                'User with given email and password does not exists'
+            )
+        return {
+            'email':user.email,
+            'token': jwt_token
+        }
+        
+###### views.py
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
+
+class UserRegistrationView(CreateAPIView):
+
+    serializer_class = UserRegistrationSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        status_code = status.HTTP_201_CREATED
+        response = {
+            'success' : 'True',
+            'status code' : status_code,
+            'message': 'User registered  successfully',
+            }
+        
+        return Response(response, status=status_code)
+
+class UserLoginView(APIView):
+
+    permission_classes = (AllowAny,)
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = {
+            'success' : 'True',
+            'status code' : status.HTTP_200_OK,
+            'message': 'User logged in  successfully',
+            'token' : serializer.data['token'],
+            }
+        status_code = status.HTTP_200_OK
+
+        return Response(response, status=status_code)
+
+
+class UserProfileView(RetrieveAPIView):
+
+    permission_classes = (IsAuthenticated,)
+    authentication_class = JSONWebTokenAuthentication
+
+    def get(self, request):
+        try:
+            user_profile = UserProfile.objects.get(user=request.user)
+            status_code = status.HTTP_200_OK
+            response = {
+                'success': 'true',
+                'status code': status_code,
+                'message': 'User profile fetched successfully',
+                'data': [{
+                    'first_name': user_profile.first_name,
+                    'last_name': user_profile.last_name,
+                    'phone_number': user_profile.phone_number,
+                    'age': user_profile.age,
+                    'gender': user_profile.gender,
+                    }]
+                }
+
+        except Exception as e:
+            status_code = status.HTTP_400_BAD_REQUEST
+            response = {
+                'success': 'false',
+                'status code': status.HTTP_400_BAD_REQUEST,
+                'message': 'User does not exists',
+                'error': str(e)
+                }
+        return Response(response, status=status_code)
+        
+##### urls.py
+from django.urls import path
+from .views import UserRegistrationView
+from .views import UserLoginView
+from .views import UserProfileView
+
+urlpatterns = [
+    path('signup/', UserRegistrationView.as_view()),
+    path('signin/', UserLoginView.as_view()),
+    path('profile/', UserProfileView.as_view()),
+
+]
+
