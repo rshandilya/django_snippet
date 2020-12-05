07b20e6d5811b208cc1768e77785e4c45beebb7b
Rahul Shandilya <prof.rshandilya@gmail.com>
2020-12-05 20:58:00 +0530

    Update views.py

--- view/views.py
+++ view/views.py
@@ -1,1 +1,25 @@
 # VIEWS SNIPPETS
+# Option 1)
+from django.shortcuts import render
+
+def detail(request,store_id='1',location=None):
+    ...
+    return render(request,'stores/detail.html', values_for_template)
+
+# Option 2)
+from django.template.response import TemplateResponse
+
+def detail(request,store_id='1',location=None):
+    ...
+    return TemplateResponse(request, 'stores/detail.html', values_for_template)
+
+# Option 3)
+from django.http import HttpResponse
+from django.template import loader, Context
+
+def detail(request,store_id='1',location=None):
+    ...
+    response = HttpResponse()
+    t = loader.get_template('stores/detail.html')
+    c = Context(values_for_template)
+    return response.write(t.render(c))
